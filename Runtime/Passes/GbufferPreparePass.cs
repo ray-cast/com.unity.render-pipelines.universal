6f56c8a385126ca347024072d71a55557c5a783b
ray-cast <2221870259@qq.com>
2021-03-06 23:02:19 +0800

    Update

--- Runtime/Passes/GbufferPreparePass.cs
+++ Runtime/Passes/GbufferPreparePass.cs
@@ -70,7 +70,7 @@ namespace UnityEngine.Rendering.Universal
             this.bufferDescriptor = new RenderTextureDescriptor[4];
             bufferDescriptor[0] = GetStereoCompatibleDescriptor(cameraTextureDescriptor, this._width, this._height, GraphicsFormat.R8G8B8A8_UNorm);
             bufferDescriptor[1] = GetStereoCompatibleDescriptor(cameraTextureDescriptor, this._width, this._height, GraphicsFormat.R8G8B8A8_UNorm);
-            bufferDescriptor[2] = GetStereoCompatibleDescriptor(cameraTextureDescriptor, this._width, this._height, GraphicsFormat.R8G8B8A8_UNorm);
+            bufferDescriptor[2] = GetStereoCompatibleDescriptor(cameraTextureDescriptor, this._width, this._height, GraphicsFormat.A2B10G10R10_UNormPack32);
             bufferDescriptor[3] = GetStereoCompatibleDescriptor(cameraTextureDescriptor, this._width, this._height, GraphicsFormat.R8G8B8A8_UNorm);
 
             _depthAttachmentHandle = depthAttachmentHandle;
